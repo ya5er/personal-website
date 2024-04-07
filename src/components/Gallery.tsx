42618b40e8cf2ee9cfcbed33c7711a65448a11de
Yaser <yayonoss@gmail.com>
2024-04-07 01:16:09 -0400

    Added scroll margin to gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,11 +1,11 @@
 export function Gallery() {
 
     return (
-        <div className="gallery w-full mx-auto flex flex-wrap justify-center mb-16">
-            <img className="shadow-md shadow-gray-300 galleryImage mb-6 mr-3" src="/gallery/gallery1.png" alt="gallery1" />
-            <img className="shadow-md shadow-gray-300 galleryImage mb-6 ml-3" src="/gallery/gallery2.png" alt="gallery2" />
-            <img className="shadow-md shadow-gray-300 galleryImage mr-3" src="/gallery/gallery3.png" alt="gallery3" />
-            <img className="shadow-md shadow-gray-300 galleryImage ml-3" src="/gallery/gallery4.JPG" alt="gallery4" />
+        <div className="scroll-mt-10 gallery w-full mx-auto flex flex-wrap justify-center mb-16" id="gallery">
+            <img className="shadow-md shadow-gray-300 galleryImage mb-6 mr-3 transition" src="/gallery/gallery1.png" alt="gallery1" />
+            <img className="shadow-md shadow-gray-300 galleryImage mb-6 ml-3 transition" src="/gallery/gallery2.png" alt="gallery2" />
+            <img className="shadow-md shadow-gray-300 galleryImage mr-3 transition" src="/gallery/gallery3.png" alt="gallery3" />
+            <img className="shadow-md shadow-gray-300 galleryImage ml-3 transition" src="/gallery/gallery4.JPG" alt="gallery4" />
         </div>
     );
 
