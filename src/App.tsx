942d97154b9495afb3bfcfe1532e461dd7df25da
Yaser <yayonoss@gmail.com>
2024-04-11 23:48:46 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ import { About } from './components/About';
 import { Gallery } from './components/Gallery';
 import { Education } from './components/Education';
 import { Experience } from './components/Experience';
-
+import { Sidebar } from './components/Sidebar';
+import { Projects } from "./components/Projects";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    <main className="bg-gray-100 h-full w-full content-center py-20 px-5">
+    <main className="bg-gray-100 h-full w-full content-center pt-20 pb-10 px-5">
+      <Sidebar />
       <About />
       <Gallery />
       <Education />
       <Experience />
+      <Projects />
+      <Footer />
     </main>
   )
 }
