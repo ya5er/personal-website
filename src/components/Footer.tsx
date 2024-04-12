2c9fb557501461a3284532caeebd6e494b2c8188
Yaser <yayonoss@gmail.com>
2024-04-11 23:49:18 -0400

    Added links to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,15 @@
 export function Footer() {
 
     return (
-        <div className="mt-16 text-center flex justify-center gap-2">
-                <a className="text-2xl mb-4 shadow-xl w-11 h-11 rounded-full bg-gray-100 flex items-center justify-center sidebar-item">
-                    <img src="/github.svg" alt="github" className="transition-transform hover:scale-110" width={21.5} height={21.5} />
+        <div className="mt-16 text-center flex justify-center gap-3">
+                <a className="text-2xl mb-4 shadow-lg w-12 h-12 rounded-full bg-gray-100 flex items-center justify-center sidebar-item" href="https://github.com/ya5er" target="_blank" rel="noopener noreferrer">
+                    <img src="/github.svg" alt="github" className="transition-transform hover:scale-110" width={25} height={25} />
                 </a>
-                <a className="text-2xl mb-4 shadow-xl w-11 h-11 rounded-full bg-gray-100 flex items-center justify-center sidebar-item">
-                    <img src="/linkedin.svg" className="transition-transform hover:scale-110" alt="linkedin" width={20} height={20} />
+                <a className="text-2xl mb-4 shadow-lg w-12 h-12 rounded-full bg-gray-100 flex items-center justify-center sidebar-item" href="https://www.linkedin.com/in/yaser-nossibeh/" target="_blank" rel="noopener noreferrer">
+                    <img src="/linkedin.svg" className="transition-transform hover:scale-110" alt="linkedin" width={23} height={23} />
                 </a>
-                <a className="text-2xl mb-4 shadow-xl w-11 h-11 rounded-full bg-gray-100 flex items-center justify-center sidebar-item">
-                    <img src="/email.svg" className="transition-transform hover:scale-110" alt="email" width={24} height={20} />
+                <a className="text-2xl mb-4 shadow-lg w-12 h-12 rounded-full bg-gray-100 flex items-center justify-center sidebar-item" href="mailto:yrynossi@uwaterloo.ca" target="_blank" rel="noopener noreferrer">
+                    <img src="/email.svg" className="transition-transform hover:scale-110" alt="email" width={27} height={21} />
                 </a>
         </div>
     );
