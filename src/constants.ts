175ddde594d4622d685357742c9ecf3af1fde6d4
Yaser <yayonoss@gmail.com>
2024-05-12 23:30:07 -0400

    CSS & Constant Updates

---
+++ src/constants.ts
@@ -0,0 +1,69 @@
+export const experience = [
+    {
+        org: "Inbenta",
+        title: "Software Developer Intern",
+        date: "Jan 2024 - Apr 2024",
+        description: "Built & optimized digital instructor platforms for global banks.",
+        skills: ["React", "JavaScript", "Redux", "PHP", "MySQL", "Laravel", "AWS (S3)", "Jenkins"],
+        logo: "/resume/inbenta.jpeg"
+    },
+    {
+        org: "Rogers Communications",
+        title: "Software Developer Intern",
+        date: "May 2023 - Sept 2023",
+        description: "Developed internal reporting & analytics tools for Wireline Network team.",
+        skills: ["Python", "TypeScript", "JavaScript", "PHP", "Laravel", "MySQL"],
+        logo: "/resume/rogers.jpeg"
+    },
+    {
+        org: "UW Blueprint",
+        title: "Software Lead",
+        date: "Sept 2023 - Present",
+        description: "Created internal tools for nonprofit organizations.",
+        skills: ["TypeScript", "Node", "Express", "React", "MongoDB", "Firebase", "Chakra UI"],
+        logo: "/resume/blueprint.jpeg"
+    },
+    {
+        org: "Midnight Sun",
+        title: "Software Developer",
+        date: "Sept 2022 - Dec 2022",
+        description: "Driver analytics platform for Strategy team.",
+        skills: ["Python", "Django", "Pandas", "NumPy", "PostgreSQL"],
+        logo: "/resume/midsun.png"
+    }
+];
+
+export const projects = [
+    {
+        name: "Medica",
+        description: "A patient management platform for physicians that allows them to easily track which medications their patients are taking while helping identify drug interactions.",
+        skills: ["Python", "Flask", "React", "JavaScript", "PostgreSQL", "Azure"],
+        image: "/projects/medica.png",
+        colour: "blue-100",
+        repo: "https://github.com/Saaniya19/Medica"
+    },
+    {
+        name: "DonoDash",
+        description: "A one-stop shop for all your donation needs. Discover charities, donate, and view your dashboard all in one site. Built during Hack the Globe, BCG’s annual hackathon.",
+        skills: ["React", "TypeScript", "Express", "Tailwind CSS", "Stripe"],
+        image: "/projects/donodash.png",
+        colour: "green-100",
+        repo: "https://github.com/Tudor-Barsan/donodash"
+    },
+    {
+        name: "Scotiabank SafeGuard",
+        description: "An AI-powered chatbot that makes reporting fraud a much quicker process. SafeGuard helps improve communication between banks and worried customers.",
+        skills: ["React", "TypeScript", "Tailwind CSS"],
+        image: "/projects/safeguard.png",
+        colour: "red-100",
+        repo: "https://github.com/tahazaryab/shacks"
+    },
+    {
+        name: "Personal Portfolio",
+        description: "A personal portfolio dedicated to my experiences and personal projects! Designed with Figma and implemented with React, TypeScript, and styled with Tailwind.",
+        skills: ["React", "TypeScript", "Tailwind CSS"],
+        image: "/projects/portfolio.png",
+        colour: "orange-200",
+        repo: "https://github.com/ya5er/personal-website"
+    }
+];
