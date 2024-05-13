73db8ad8be84c8480ed5fd28ce07a12ac679ecf9
Yaser <yayonoss@gmail.com>
2024-05-12 23:31:04 -0400

    Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    'bg-red-100',
+    'bg-green-100',
+    'bg-blue-100',
+    'bg-orange-200',
+    'bg-gray-100',
+    'hover:shadow-red-100',
+    'hover:shadow-green-100',
+    'hover:shadow-blue-100',
+    'hover:shadow-orange-200',
+    'hover:shadow-gray-100',
+  ],
   theme: {
     extend: {},
   },
