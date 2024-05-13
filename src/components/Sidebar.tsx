6efee90ef544b6bf72c1e8f76d768aec66cddbc5
Yaser <yayonoss@gmail.com>
2024-05-12 23:29:13 -0400

    Created Sidebar Component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+export function Sidebar() {
+    return (
+        <div className="fixed ml-4 top-1/2 transform -translate-y-1/2 items-center justify-center bg-white rounded-full drop-shadow-md sidebar">
+            <a href="#about" className="text-2xl my-1 w-12 h-12 flex items-center justify-center sidebar-item">&#128075;</a>
+            <a href="#gallery" className="text-2xl my-1 w-12 h-12 flex items-center justify-center sidebar-item">&#128248;</a>
+            <a href="#education" className="text-2xl my-1 w-12 h-12 flex items-center justify-center sidebar-item">&#128218;</a>
+            <a href="#experience" className="text-2xl my-1 w-12 h-12 flex items-center justify-center sidebar-item">&#128188;</a>
+            <a href="#projects" className="text-2xl my-1 w-12 h-12 flex items-center justify-center sidebar-item">&#128187;</a>
+        </div>
+    );
+}
+
+
